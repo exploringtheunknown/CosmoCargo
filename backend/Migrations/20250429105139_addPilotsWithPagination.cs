56048c8707abeb41f93b1ecaeaa405cb722720c6
SebastianEstrada <sebastian@queenslab.se>
2025-04-29 13:16:26 +0200

    feat: migrated db

---
+++ backend/Migrations/20250429105139_addPilotsWithPagination.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CosmoCargo.Migrations
+{
+    /// <inheritdoc />
+    public partial class addPilotsWithPagination : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "status",
+                table: "users",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "updated_at",
+                table: "users",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "status",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "updated_at",
+                table: "users");
+        }
+    }
+}
